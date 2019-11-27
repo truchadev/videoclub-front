201141e8b96b2a51741d399cbe77629c1c8b90c9
Laura Cruz <lcc4413@gmail.com>
2019-11-27 18:42:36 +0100

    Update movie-categories.component.ts

--- src/app/components/movie-categories/movie-categories.component.ts
+++ src/app/components/movie-categories/movie-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movies } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movie-categories',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieCategoriesComponent implements OnInit {
 
-  constructor() { }
+  category: string;
+  movies: Array<Movies>;
+;  constructor() { }
 
   ngOnInit() {
   }
